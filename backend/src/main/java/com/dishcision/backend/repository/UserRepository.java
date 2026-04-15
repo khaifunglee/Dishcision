20349e02a78d5951990920ad61c91e1295ec2b26
khaifunglee <leekhaifung@gmail.com>
2026-04-15 17:32:13 +1000

    create user repository

--- backend/src/main/java/com/dishcision/backend/repository/UserRepository.java
+++ backend/src/main/java/com/dishcision/backend/repository/UserRepository.java
@@ -1,5 +1,13 @@
+// This interface defines DB queries for 'user' table by writing method names
 package com.dishcision.backend.repository;
 
-public class UserRepository {
+import com.dishcision.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository; // automatically gives save(), findByID(), delete(), etc.
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    // Define custom queries
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
