46a3fa7e6e3f7fafef5210a3578d8871f80664e4
khaifunglee <leekhaifung@gmail.com>
2026-04-16 15:35:48 +1000

    created AuthController to handle incoming HTTP requests

--- README.md
+++ README.md
@@ -10,3 +10,15 @@ Framework: React Native + Expo (Frontend), Spring Boot (Backend)
 - Created colour scheme for light/dark themed colours
 - Created themed components to allow for easier templating for the app's text, cards, images, etc.
 - Added route groups & nested layouts by creating Login & Register page
+
+### 14-04-2026
+**Added simple React Native features**:
+- Added themed pressable components for easier templating for buttons
+- Added bottom tab bar for home dashboard pages
+- Added safe area view into themed view component
+
+### 15-04-2026
+**Setting up Backend and Database**:
+- Created PostgreSQL database and setup Spring Boot backend
+- Added essential dependencies for user authentication
+- Configured database connection to connect to backend server
--- backend/src/main/java/com/dishcision/backend/controller/AuthController.java
+++ backend/src/main/java/com/dishcision/backend/controller/AuthController.java
@@ -1,5 +1,33 @@
+// This file handles incoming HTTP register/login requests 
 package com.dishcision.backend.controller;
+
+import com.dishcision.backend.dto.*;
+import com.dishcision.backend.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
 
 public class AuthController {
 
+    private final AuthService authService;
+
+    // Register request
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@Valid @RequestBody RegisterRequest request) {
+        String token = authService.register(request);
+        return ResponseEntity.ok(Map.of("token", token));
+    }
+
+    // Login request
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@Valid @RequestBody LoginRequest request) {
+        String token = authService.login(request);
+        return ResponseEntity.ok(Map.of("token", token));
+    }
 }
