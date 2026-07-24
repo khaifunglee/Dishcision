338e78fa1e949b0f5aeca07424355ca04f4c1f37
khaifunglee <leekhaifung@gmail.com>
2026-07-24 19:34:53 +1000

    fix: reverted metro config's expo extension arrays to default

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -2,6 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config')
 
 const config = getDefaultConfig(__dirname)
 
-config.resolver.sourceExts = ['jsx', 'js', 'ts', 'tsx', 'json']
-
 module.exports = config
